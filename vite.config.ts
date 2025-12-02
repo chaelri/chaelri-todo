c7e55ee010dbb1ea29ca813c519578e587eee089
chaelri <charliecayno@gmail.com>
2025-12-02 13:05:31 +0800

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
   base: "/chaelri-todo/",
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+    copyPublicDir: true,
+  },
 });
