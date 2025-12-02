d92b63f19703c089effc0288dbc27da72a8dd834
chaelri <charliecayno@gmail.com>
2025-12-02 11:36:59 +0800

    add firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getStorage } from "firebase/storage";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAzbXjGyT1_jbpIdJYKYdcG9y368r7NKRw",
+  authDomain: "chaelri-todo.firebaseapp.com",
+  projectId: "chaelri-todo",
+  storageBucket: "chaelri-todo.firebasestorage.app",
+  messagingSenderId: "271724667038",
+  appId: "1:271724667038:web:185cd2c2db593b96db9b8f"
 };
 
 const app = initializeApp(firebaseConfig);
