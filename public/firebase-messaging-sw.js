68c778b8d433a0b0a5a3a8ed08c02e77298e7059
charliecayno <charliecayno@gmail.com>
2025-12-03 00:18:06 +0800

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,28 +1,36 @@
 // Firebase Messaging Service Worker
-importScripts("https://www.gstatic.com/firebasejs/10.13.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.13.0/firebase-messaging-compat.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.13.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.13.0/firebase-messaging-compat.js"
+);
 
 // SAME Firebase config as firebase.ts
 firebase.initializeApp({
-    apiKey: "AIzaSyAzbXjGyT1_jbpIdJYKYdcG9y368r7NKRw",
-    authDomain: "chaelri-todo.firebaseapp.com",
-    projectId: "chaelri-todo",
-    storageBucket: "chaelri-todo.firebasestorage.app",
-    messagingSenderId: "271724667038",
-    appId: "1:271724667038:web:185cd2c2db593b96db9b8f"
+  apiKey: "AIzaSyAzbXjGyT1_jbpIdJYKYdcG9y368r7NKRw",
+  authDomain: "chaelri-todo.firebaseapp.com",
+  projectId: "chaelri-todo",
+  storageBucket: "chaelri-todo.firebasestorage.app",
+  messagingSenderId: "271724667038",
+  appId: "1:271724667038:web:185cd2c2db593b96db9b8f",
 });
 
 // Background push handling
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    self.registration.showNotification(
-        payload.notification?.title || "Chaelri ToDo",
-        {
-            body: payload.notification?.body || "",
-            icon: "/icon-192.png"
-        }
-    );
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+
+  const notificationTitle = payload.data.title;
+  const notificationOptions = {
+    body: payload.data.body,
+    icon: "/icon-192.png",
+    image: payload.data.imageUrl,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
-
-
